87f3f64428fc81654d07453573b68f756ff78eae
Vasusund <vasusund1977@gmail.com>
2025-09-08 00:28:40 +1000

    Adding index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log("Sample App Running...");
+
+module.exports = { add, subtract };
+console.log("Hello World!")
